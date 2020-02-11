73349276d3c74ead28b680777fd8d014b8f119cf
veronica larrondo <vero.larrondo.r@gmail.com>
2020-02-11 16:27:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor (props){
+    super (props);
+    this.state ={         
+        color: null,
+    }
+}
+  
+render (){ 
+  console.log(this.state)
+  let redOn = '';
+  if(this.state.color === 'red') redOn = 'selected';      
+  let yellowOn = '';
+  if(this.state.color === 'yellow') yellowOn = 'selected';     
+  let greenOn = '';
+  if(this.state.color === 'green') greenOn = 'selected';
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='stick mx-auto d-block'></div>
+      <div className='traffic mx-auto p-2'>                   
+        <span className={"red light d-block rounded-circle my-3 mx-auto " + redOn} onClick={()=>this.setState({color: 'red'})}></span>        
+        <span  className={"yellow light d-block rounded-circle my-3 mx-auto " + yellowOn} onClick={()=>this.setState({color: 'yellow'})}></span>      
+        <span  className={"green light d-block rounded-circle my-3 mx-auto bg-success " + greenOn} onClick={()=>this.setState({color: 'green'})}></span>                
+      </div>
     </div>
   );
+}
 }
 
 export default App;
